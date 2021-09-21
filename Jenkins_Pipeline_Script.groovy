42796a0ac8597e086d59814dc0dbd0f700e624ae
Narayanad419 <89384803+Narayanad419@users.noreply.github.com>
2021-09-21 19:07:55 +0530

    Update Jenkins_Pipeline_Script.groovy

--- Jenkins_Pipeline_Script.groovy
+++ Jenkins_Pipeline_Script.groovy
@@ -1,5 +1,7 @@
+currentBuild.displayName="Pipeline2-#"+BUILD_NUMBER
 pipeline {
     agent {label 'Slave'}
+    environment {PATH = "/opt/maven/bin:$PATH"}
     stages {
         stage('Hello') {
             steps {
@@ -12,6 +14,18 @@ pipeline {
                 branches: [[name: '*/master']],
                 extensions: [],
                 userRemoteConfigs: [[url: 'https://github.com/Narayanad419/hello-world-war.git']]])
+            }
+        }
+        stage('build'){
+            steps {
+                sh '''mvn package'''
+            }
+        }
+        stage('deploy'){
+            steps {
+                deploy adapters: [tomcat8(credentialsId: 'tomcat', path: '',
+                url: 'http://3.109.219.27:8080/')],
+                contextPath: 'Hello', war: '**target/*.war'
             }
         }
     }
